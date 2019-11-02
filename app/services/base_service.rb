3f53758bad639ee4bf1befd87934fbf669520e01
Sathish kumar N <nnsathish@gmail.com>
2019-11-02 20:49:59 +0530

    Enhance services for better reporting and handling

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -17,11 +17,27 @@ class BaseService
     !self.success?
   end
 
-  def set_error!(error_msg)
-    self.response[:error_code] = error_msg.to_s
+  def set_error!(error_code)
+    self.response[:error_code] = error_code
+    self
   end
 
   def error
     self.response[:error_code]
+  end
+
+  def set_data!(data)
+    self.response[:data] = data
+    self
+  end
+
+  def data
+    self.response[:data]
+  end
+
+  def log_exception(ex)
+    msg = "\nEXCEPTION::#{self.class.name} - #{ex.message}\nBACKTRACE:\n"
+    msg << "#{Rails.backtrace_cleaner.clean(ex.backtrace).join("\n")}\n"
+    Rails.logger.error(msg)
   end
 end
--- app/services/fetch_recipes_service.rb
+++ app/services/fetch_recipes_service.rb
@@ -1,15 +1,23 @@
 class FetchRecipesService < BaseService
-  attr_reader :wrapper
+  attr_reader :options
 
   def initialize(options = {})
     super
-    @wrapper = ContentfulClientWrapper.new(options)
+    @options = options
   end
 
   def call
     recipes = wrapper.recipes
-  rescue StandardError
+    set_data!(recipes)
+  rescue StandardError => ex
+    log_exception(ex)
     set_error!(:fetch_error)
-    {}
+    set_data!([])
+  end
+
+  private
+
+  def wrapper
+    @wrapper ||= ContentfulClientWrapper.new(options)
   end
 end
